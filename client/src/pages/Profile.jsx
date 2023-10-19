ec4a91cc889142b820ba879936e840693aaf1224
huss-huss <husibest000@gmail.com>
2023-10-19 16:42:59 +0300

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,6 +18,7 @@ import {
   updateUserStart,
   updateUserSuccess,
 } from '../redux/user/userSlice'
+import { Link } from 'react-router-dom'
 
 const Profile = () => {
   const { currentUser, loading, error } = useSelector((state) => state.user)
@@ -185,6 +186,12 @@ const Profile = () => {
         >
           {loading ? 'Loading...' : 'Update Profile'}
         </button>
+        <Link
+          to={'/create-listing'}
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95 disabled:opacity-80"
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDelete} className="text-red-700 cursor-pointer ">
