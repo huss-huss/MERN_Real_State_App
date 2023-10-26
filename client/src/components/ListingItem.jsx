7f5353d33adffb0faf3864cc93546d938a527bbc
huss-huss <husibest000@gmail.com>
2023-10-26 11:43:48 +0300

    complete home page

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -5,7 +5,7 @@ import { MdLocationOn } from 'react-icons/md'
 const ListingItem = ({ listing }) => {
   return (
     <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
-      <Link to={`listings/${listing._id}`}>
+      <Link to={`/listings/${listing._id}`}>
         <img
           src={listing.imageUrls[0]}
           alt="listing cover"
